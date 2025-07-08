31a3ebb1d7a455799b0963e2245a31b89f379aad
Sample First <132287607+SampleFirst@users.noreply.github.com>
2025-07-08 23:13:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -373,160 +373,198 @@ async def admin_filter(filt, client, message):
     return await admin_check(message)
 
 
-async def get_verify_shorted_link(num, link):
-    if int(num) == 1:
-        API = VERIFY1_API
-        URL = VERIFY1_URL
-    else:
-        API = VERIFY2_API
-        URL = VERIFY2_URL
-    https = link.split(":")[0]
-    if "http" == https:
-        https = "https"
-        link = link.replace("http", https)
-
-    if URL == "api.shareus.in":
-        url = f"https://{URL}/shortLink"
-        params = {"token": API,
-                  "format": "json",
-                  "link": link,
-                  }
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                    data = await response.json(content_type="text/html")
-                    if data["status"] == "success":
-                        return data["shortlink"]
-                    else:
-                        logger.error(f"Error: {data['message']}")
-                        return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
-
-        except Exception as e:
-            logger.error(e)
-            return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
-    else:
-        url = f'https://{URL}/api'
-        params = {'api': API,
-                  'url': link,
-                  }
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                    data = await response.json()
-                    if data["status"] == "success":
-                        return data["shortenedUrl"]
-                    else:
-                        logger.error(f"Error: {data['message']}")
-                        if URL == 'clicksfly.com':
-                            return f'https://{URL}/api?api={API}&url={link}'
+async def get_verify_shorted_link(bot, num, link):
+    try:
+        if int(num) == 1:
+            API = VERIFY1_API
+            URL = VERIFY1_URL
+        else:
+            API = VERIFY2_API
+            URL = VERIFY2_URL
+        https = link.split(":")[0]
+        if "http" == https:
+            https = "https"
+            link = link.replace("http", https)
+    
+        if URL == "api.shareus.in":
+            url = f"https://{URL}/shortLink"
+            params = {"token": API,
+                      "format": "json",
+                      "link": link,
+                      }
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                        data = await response.json(content_type="text/html")
+                        if data["status"] == "success":
+                            return data["shortlink"]
                         else:
-                            return f'https://{URL}/api?api={API}&link={link}'
-        except Exception as e:
-            logger.error(e)
-            if URL == 'clicksfly.com':
-                return f'https://{URL}/api?api={API}&url={link}'
-            else:
-                return f'https://{URL}/api?api={API}&link={link}'
+                            logger.error(f"Error: {data['message']}")
+                            return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
+    
+            except Exception as e:
+                logger.error(e)
+                return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
+        else:
+            url = f'https://{URL}/api'
+            params = {'api': API,
+                      'url': link,
+                      }
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                        data = await response.json()
+                        if data["status"] == "success":
+                            return data["shortenedUrl"]
+                        else:
+                            logger.error(f"Error: {data['message']}")
+                            if URL == 'clicksfly.com':
+                                return f'https://{URL}/api?api={API}&url={link}'
+                            else:
+                                return f'https://{URL}/api?api={API}&link={link}'
+            except Exception as e:
+                logger.error(e)
+                if URL == 'clicksfly.com':
+                    return f'https://{URL}/api?api={API}&url={link}'
+                else:
+                    return f'https://{URL}/api?api={API}&link={link}'
+    except Exception as e:
+        logging.error(f"❌ Error occurred while verifying user {userid}: {e}", exc_info=True)
+        # Optional: send a message to admin or log channel
+        await bot.send_message(LOG_CHANNEL, f"⚠️ Error verifying user `{userid}`:\n`{str(e)}`")
 
 async def check_token(bot, userid, token):
-    user = await bot.get_users(userid)
-    if not await db.is_user_exist(user.id):
-        await db.add_user(user.id, user.first_name)
-        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
-    if user.id in TOKENS.keys():
-        TKN = TOKENS[user.id]
-        if token in TKN.keys():
-            is_used = TKN[token]
-            if is_used == True:
-                return False
-            else:
-                return True
-    else:
-        return False
+    try:
+        user = await bot.get_users(userid)
+        if not await db.is_user_exist(user.id):
+            await db.add_user(user.id, user.first_name)
+            await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+        if user.id in TOKENS.keys():
+            TKN = TOKENS[user.id]
+            if token in TKN.keys():
+                is_used = TKN[token]
+                if is_used == True:
+                    return False
+                else:
+                    return True
+        else:
+            return False
+    except Exception as e:
+        logging.error(f"❌ Error occurred while verifying user {userid}: {e}", exc_info=True)
+        # Optional: send a message to admin or log channel
+        await bot.send_message(LOG_CHANNEL, f"⚠️ Error verifying user `{userid}`:\n`{str(e)}`")
 
 async def get_token(bot, userid, link, fileid):
-    user = await bot.get_users(userid)
-    if not await db.is_user_exist(user.id):
-        await db.add_user(user.id, user.first_name)
-        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
-    token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
-    TOKENS[user.id] = {token: False}
-    url = f"{link}verify-{user.id}-{token}-{fileid}"
-    status = await get_verify_status(user.id)
-    date_var = status["date"]
-    time_var = status["time"]
-    hour, minute, second = time_var.split(":")
-    year, month, day = date_var.split("-")
-    last_date, last_time = str((datetime(year=int(year), month=int(month), day=int(day), hour=int(hour), minute=int(minute), second=int(second)))-timedelta(hours=12)).split(" ")
-    tz = pytz.timezone('Asia/Kolkata')
-    curr_date, curr_time = str(datetime.now(tz)).split(" ")
-    if last_date == curr_date:
-        vr_num = 2
-    else:
-        vr_num = 1
-    shortened_verify_url = await get_verify_shorted_link(vr_num, url)
-    return str(shortened_verify_url)
+    try:
+        user = await bot.get_users(userid)
+        if not await db.is_user_exist(user.id):
+            await db.add_user(user.id, user.first_name)
+            await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+        token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+        TOKENS[user.id] = {token: False}
+        url = f"{link}verify-{user.id}-{token}--{fileid}"
+        status = await get_verify_status(bot, user.id)
+        date_var = status["date"]
+        time_var = status["time"]
+        hour, minute, second = time_var.split(":")
+        year, month, day = date_var.split("-")
+        last_date, last_time = str((datetime(year=int(year), month=int(month), day=int(day), hour=int(hour), minute=int(minute), second=int(second)))-timedelta(hours=12)).split(" ")
+        tz = pytz.timezone('Asia/Kolkata')
+        curr_date, curr_time = str(datetime.now(tz)).split(" ")
+        if last_date == curr_date:
+            vr_num = 2
+        else:
+            vr_num = 1
+        shortened_verify_url = await get_verify_shorted_link(bot, baavr_num, url)
+        return str(shortened_verify_url)
+    except Exception as e:
+        logging.error(f"❌ Error occurred while verifying user {userid}: {e}", exc_info=True)
+        # Optional: send a message to admin or log channel
+        await bot.send_message(LOG_CHANNEL, f"⚠️ Error verifying user `{userid}`:\n`{str(e)}`")
 
 
-async def get_verify_status(userid):
-    status = temp.VERIFY.get(userid)
-    if not status:
-        status = await db.get_verified(userid)
-        temp.VERIFY[userid] = status
-    return status
-    
+async def get_verify_status(bot, userid):
+    try:
+        status = temp.VERIFY.get(userid)
+        if not status:
+            status = await db.get_verified(userid)
+            temp.VERIFY[userid] = status
+        return status
+    except Exception as e:
+        logging.error(f"❌ Error occurred while verifying user {userid}: {e}", exc_info=True)
+        # Optional: send a message to admin or log channel
+        await bot.send_message(LOG_CHANNEL, f"⚠️ Error verifying user `{userid}`:\n`{str(e)}`")
+
 async def send_verification_log(bot, userid, date_temp, time_temp):
-    user = await bot.get_users(int(userid))
-    log_message = f"#VerificationLog:\nUser ID: {user.id}\nUser Name: {user.mention}\nDate: {date_temp}\nTime: {time_temp}"
-    await bot.send_message(LOG_CHANNEL, log_message)
-
+    try:
+        user = await bot.get_users(int(userid))
+        log_message = f"#VerificationLog:\nUser ID: {user.id}\nUser Name: {user.mention}\nDate: {date_temp}\nTime: {time_temp}"
+        await bot.send_message(LOG_CHANNEL, log_message)
+    except Exception as e:
+        logging.error(f"❌ Error occurred while verifying user {userid}: {e}", exc_info=True)
+        # Optional: send a message to admin or log channel
+        await bot.send_message(LOG_CHANNEL, f"⚠️ Error verifying user `{userid}`:\n`{str(e)}`")
 
 async def update_verify_status(bot, userid, date_temp, time_temp):
-    status = await get_verify_status(userid)
-    status["date"] = date_temp
-    status["time"] = time_temp
-    temp.VERIFY[userid] = status
-    await db.update_verification(userid, date_temp, time_temp)
-    await send_verification_log(bot, userid, date_temp, time_temp)
+    try:
+        status = await get_verify_status(bot, userid)
+        status["date"] = date_temp
+        status["time"] = time_temp
+        temp.VERIFY[userid] = status
+        await db.update_verification(userid, date_temp, time_temp)
+        await send_verification_log(bot, userid, date_temp, time_temp)
+    except Exception as e:
+        logging.error(f"❌ Error occurred while verifying user {userid}: {e}", exc_info=True)
+        # Optional: send a message to admin or log channel
+        await bot.send_message(LOG_CHANNEL, f"⚠️ Error verifying user `{userid}`:\n`{str(e)}`")
 
 async def verify_user(bot, userid, token):
-    user = await bot.get_users(int(userid))
-    if not await db.is_user_exist(user.id):
-        await db.add_user(user.id, user.first_name)
-        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
-    TOKENS[user.id] = {token: True}
-    tz = pytz.timezone('Asia/Kolkata')
-    date_var = datetime.now(tz)+timedelta(hours=12)
-    temp_time = date_var.strftime("%H:%M:%S")
-    date_var, time_var = str(date_var).split(" ")
-    await update_verify_status(bot, user.id, date_var, temp_time)
+    try:
+        user = await bot.get_users(int(userid))
+        if not await db.is_user_exist(user.id):
+            await db.add_user(user.id, user.first_name)
+            await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+        TOKENS[user.id] = {token: True}
+        tz = pytz.timezone('Asia/Kolkata')
+        date_var = datetime.now(tz)+timedelta(hours=12)
+        temp_time = date_var.strftime("%H:%M:%S")
+        date_var, time_var = str(date_var).split(" ")
+        await update_verify_status(bot, user.id, date_var, temp_time)
+    except Exception as e:
+            logging.error(f"❌ Error occurred while verifying user {userid}: {e}", exc_info=True)
+            # Optional: send a message to admin or log channel
+            await bot.send_message(LOG_CHANNEL, f"⚠️ Error verifying user `{userid}`:\n`{str(e)}`")
 
 async def check_verification(bot, userid):
-    user = await bot.get_users(int(userid))
-    if not await db.is_user_exist(user.id):
-        await db.add_user(user.id, user.first_name)
-        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
-    tz = pytz.timezone('Asia/Kolkata')
-    today = date.today()
-    now = datetime.now(tz)
-    curr_time = now.strftime("%H:%M:%S")
-    hour1, minute1, second1 = curr_time.split(":")
-    curr_time = time(int(hour1), int(minute1), int(second1))
-    status = await get_verify_status(user.id)
-    date_var = status["date"]
-    time_var = status["time"]
-    years, month, day = date_var.split('-')
-    comp_date = date(int(years), int(month), int(day))
-    hour, minute, second = time_var.split(":")
-    comp_time = time(int(hour), int(minute), int(second))
-    if comp_date<today:
-        return False
-    else:
-        if comp_date == today:
-            if comp_time<curr_time:
-                return False
+    try:
+        user = await bot.get_users(int(userid))
+        if not await db.is_user_exist(user.id):
+            await db.add_user(user.id, user.first_name)
+            await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+        tz = pytz.timezone('Asia/Kolkata')
+        today = date.today()
+        now = datetime.now(tz)
+        curr_time = now.strftime("%H:%M:%S")
+        hour1, minute1, second1 = curr_time.split(":")
+        curr_time = time(int(hour1), int(minute1), int(second1))
+        status = await get_verify_status(bot, user.id)
+        date_var = status["date"]
+        time_var = status["time"]
+        years, month, day = date_var.split('-')
+        comp_date = date(int(years), int(month), int(day))
+        hour, minute, second = time_var.split(":")
+        comp_time = time(int(hour), int(minute), int(second))
+        if comp_date<today:
+            return False
+        else:
+            if comp_date == today:
+                if comp_time<curr_time:
+                    return False
+                else:
+                    return True
             else:
                 return True
-        else:
-            return True
-
+    except Exception as e:
+        logging.error(f"❌ Error occurred while verifying user {userid}: {e}", exc_info=True)
+        # Optional: send a message to admin or log channel
+        await bot.send_message(LOG_CHANNEL, f"⚠️ Error verifying user `{userid}`:\n`{str(e)}`")
