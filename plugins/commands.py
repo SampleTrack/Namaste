a80bd91b34fa632cccc6400c6bf6ff829637a017
Sample First <132287607+SampleFirst@users.noreply.github.com>
2025-07-25 01:12:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -276,13 +276,7 @@ async def start(client, message):
 
     files_ = await get_file_details(file_id)           
     if not files_:
-        try:
-            decoded_data = base64.urlsafe_b64decode(data + "=" * (-len(data) % 4)).decode("ascii")
-            pre, file_id = decoded_data.split("_", 1)
-        except Exception as e:
-            logger.exception("Base64 decoding failed!")
-            return await message.reply("⚠️ Invalid or expired file link. Please check the link and try again.")
-        
+        pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         try:
             if IS_VERIFY and not await check_verification(client, message.from_user.id):
                 btn = [[
