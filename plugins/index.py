7fdf4f41e3051f001d0b98fdc70807b79d14fd59
SampleTrack <FirstSample@proton.me>
2026-05-17 23:10:45 +0530

    Fix regex pattern for link matching

--- plugins/index.py
+++ plugins/index.py
@@ -46,7 +46,7 @@ async def index_files(bot, query):
 @Client.on_message((filters.forwarded | (filters.regex(r"(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text ) & filters.private & filters.incoming & filters.user(ADMINS))
 async def send_for_index(bot, message):
     if message.text:
-        regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
+        regex = re.compile(r"(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
         match = regex.match(message.text)
         if not match: return await message.reply('Invalid link')
         chat_id = match.group(4)
